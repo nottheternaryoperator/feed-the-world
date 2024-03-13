1a4b045d053c7af605b817122a56b25bb5396e68
Zeus <grahamy2711@yahoo.com>
2024-03-13 12:01:51 +0000

    restyled connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/mongoDB');
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/index-DB'
+);
 
 module.exports = mongoose.connection;
