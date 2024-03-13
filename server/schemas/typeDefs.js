b7e0f6b9214bc6e44470ad045c8ff20aaf9acd22
Lllewisreynolds <lsreynolds108@gmail.com>
2024-03-13 19:01:20 +0000

    typeDefs file populated based on current User model to define overall Schema of GraphQL API as it currently stands

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,27 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    user(userId: ID!): User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
