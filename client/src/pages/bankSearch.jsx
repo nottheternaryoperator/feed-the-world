b2c90118db5448a8ff3997d5fb7ff677914ceacd
Tomasz Pawlikowski <tp4458@gmail.com>
2024-03-15 15:31:41 +0000

    writeen API call for foodbanks

--- client/src/pages/bankSearch.jsx
+++ client/src/pages/bankSearch.jsx
@@ -12,4 +12,38 @@ const BankSearch = () => {
   useEffect(() => {
     return () => savedBankIds(savedBankIds);
   });
+
+  const formSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    if (!searchInput) {
+      return false;
+    }
+
+    try {
+      const response = await fetch(
+        `https://www.givefood.org.uk/api/2/foodbanks/search/?address=${searchInput}` //put API in here
+      );
+      if (!response.ok) {
+        throw new Error('API response error');
+      }
+
+      const { results } = await response.json();
+
+      const bankData = results.map((bank) => ({
+        // bankId: bank.id,
+        name: bank.name,
+        address: bank.address,
+        needs: bank.needs || ["This foodbank hasn't posted any needed items."],
+        phone: bank.phone || ['no phone number available.'],
+        email: bank.email || ['no email address available.'],
+        link: bank.urls.homepage || ['no website address available'],
+      }));
+
+      setSearchedBanks(bankData);
+      setSearchInput('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
 };
