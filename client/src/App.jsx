dcced49abc5bc973bb1475bcd7e1ea8fd2f45f6f
Zeus <grahamy2711@yahoo.com>
2024-03-15 10:15:48 +0000

    amended App.jsx & Main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,55 @@
+import './App.css';
+import { Outlet } from 'react-router-dom';
 import Navbar from "./components/Navbar/navbar.jsx";
-import Login from "./components/login/Login.jsx";
-import Signup from "./components/login/SignUp.jsx";
-import Footer from "./components/Footer/footer.jsx";
+// import Login from "./components/login/Login.jsx";
+// import Signup from "./components/login/SignUp.jsx";
+// import Footer from "./components/Footer/footer.jsx";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-    return (
-      <div className="card">
-          <Navbar />
-          <Login />
-          <Signup />
-          <Footer />
-      </div>
+  return (
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
+
+// function App() {
+//     return (
+//       <div className="card">
+//           <Navbar />
+//           <Login />
+//           <Signup />
+//           <Footer />
+//       </div>
+//   );
 }
 
 export default App
