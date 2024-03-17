4107b4be98fa796de2ac25f6f2dcf7d628c59c40
Tomasz Pawlikowski <tp4458@gmail.com>
2024-03-17 16:28:33 +0000

    savedBanks.jsx and css created, imports added

---
+++ client/src/pages/savedBanks.jsx
@@ -0,0 +1,20 @@
+import { useQuery, useMutation } from '@apollo/client';
+import { QUERY_ME } from '../utils/queries';
+import { REMOVE_BANK } from '../utils/mutations';
+import Auth from '../utils/auth';
+import { removeBankName } from '../utils/localStorage';
+
+import TextField from '@mui/material/TextField';
+import {
+  CardActionArea,
+  FormControl,
+  FormLabel,
+  CardActions,
+  CardContent,
+  Typography,
+} from '@mui/material';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import './savedBanks.css';
