f07d1d02bc1c9e459c9703f4fdf735a88f328da1
Aliriza Bagdat <ambagdat@gmail.com>
2024-03-21 17:57:19 +0000

    final styling for the project

--- client/src/components/form/Navbar.jsx
+++ client/src/components/form/Navbar.jsx
@@ -50,10 +50,10 @@ const AppNavbar = () => {
             <Modal.Title id="signup-modal">
               <Nav variant="pills">
                 <Nav.Item >
-                  <Nav.Link eventKey="login">Login</Nav.Link>
+                  <Nav.Link eventKey="login" className='logbut'>Login</Nav.Link>
                 </Nav.Item>
                 <Nav.Item >
-                  <Nav.Link eventKey="signup" >Sign Up</Nav.Link>
+                  <Nav.Link eventKey="signup" className='logbut'>Sign Up</Nav.Link>
                 </Nav.Item>
               </Nav>
             </Modal.Title>
