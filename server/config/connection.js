eaf5b4a9ebfa8a93dd535d42efcfc925e48a0353
Zeus <grahamy2711@yahoo.com>
2024-03-21 21:15:11 +0000

    corrected connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/foodbank-db'
+  process.env.MONGODB_URI 
 );
 
 module.exports = mongoose.connection;
